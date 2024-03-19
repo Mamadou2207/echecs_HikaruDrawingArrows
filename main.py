5ca8bf892b5eb9a5a701493e8e79b6acee22ab22
Jus2p0m <128704669+Jus2p0m@users.noreply.github.com>
2024-03-19 14:32:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,72 @@
 import variables_pieces
 
-t = [] # Tableau pour l'échiquier
+t = [] # Tableau représentant l'échiquier
 
 def creer_echiquier(t):
     """Crée un échiquier à l'aide d'un tableau t
 
     Args:
-        t (str): Tableau
+        t (list): Tableau à 2 dimensions
     """
-    for loop in range(19):                    # Répéter 19 fois :
-        ligne = []                                # Tableau ligne dans t
-        for loop in range(19):                    # Répéter 19 fois :
-                ligne.append(" ")                     # Nouvel indice ayant pour valeur " "
-        t.append(ligne)                           # Nouvel indice du tableau t ayant pour valeur un tableau ligne
-    for x in range(19):                       # Répéter 19 fois :
-        for y in range(19):                       # Répéter 19 fois :
-            if y%2 == 0:                              # Si le reste de y par 2 != à 0 :
-                t[x][y] = "|"                             # Dessine quadrillage vertical
-                y *= 3                                    # Décalage permettant le quadrillage (Multiplier par 3 à chaque boucle)
-            elif x%2 == 0 and t[x][y] == " ":         # Si le reste de x par 2 != à 0 et que la valeur = " " :
-                t[x][y] = "-"                             # Dessine quadrillage horizontal
-    for i in range(8):                        # Répéter 8 fois :
-        i += 1                                    # Ajouter 1 à chaque boucle
-        t[i*2-1][1] = 9-i                         # Affiche les coordonnées y (axe des ordonnées) 
-        t[17][i*2+1] = chr(ord("A")+i-1)          # Affiche les coordonnées x (axe des abscisses) 
-        t[3][i*2+1] = "♟︎"                        # Dessine Pion Noir
-        t[13][i*2+1] = "♙"                       # Dessine Pion Blanc
-    t[15][3] = "♖"                           # Dessine Tour Blanche Gauche
-    t[15][17] = "♖"                          # Dessine Tour Blanche Droite
-    t[15][5] = "♘"                           # Dessine Cavalier Blanc Gauche
-    t[15][15] = "♘"                          # Dessine Cavalier Blanc Droite
-    t[15][7] = "♗"                           # Dessine Fou Blanc Gauche
-    t[15][13] = "♗"                          # Dessine Fou Blanc Droite
-    t[15][9] = "♕"                           # Dessine Reine Blanche
-    t[15][11] = "♔"                          # Dessine Roi Blanc
-    t[1][3] = "♜"                            # Dessine Tour Noir Gauche
-    t[1][17] = "♜"                           # Dessine Tour Noir Droite
-    t[1][5] = "♞"                            # Dessine Cavalier Noir Gauche
-    t[1][15] = "♞"                           # Dessine Cavalier Noir Droite
-    t[1][7] = "♝"                            # Dessine Fou Noir Gauche
-    t[1][13] = "♝"                           # Dessine Fou Noir Droite
-    t[1][9] = "♚"                            # Dessine Roi Noir
-    t[1][11] = "♛"                           # Dessine Reine Blanche
+    for loop in range(19):                     # Répéter 19 fois :
+        ligne = []                                 # Tableau ligne dans t
+        for loop in range(19):                     # Répéter 19 fois :
+                ligne.append(" ")                      # Nouvel indice ayant pour valeur " "
+        t.append(ligne)                            # Nouvel indice du tableau t ayant pour valeur un tableau ligne
+    for x in range(19):                        # Répéter 19 fois :
+        for y in range(19):                        # Répéter 19 fois :
+            if y%2 == 0:                               # Si le reste de y par 2 != à 0 :
+                t[x][y] = "|"                              # Dessine quadrillage vertical
+                y *= 3                                     # Décalage permettant le quadrillage (Multiplier par 3 à chaque boucle)
+            elif x%2 == 0 and t[x][y] == " ":          # Si le reste de x par 2 != à 0 et que la valeur = " " :
+                t[x][y] = "-"                              # Dessine quadrillage horizontal
+    for i in range(8):                         # Répéter 8 fois :
+        i += 1                                     # Ajouter 1 à chaque boucle
+        t[i*2-1][1] = 9-i                          # Affiche les coordonnées y (axe des ordonnées) 
+        t[17][i*2+1] = chr(ord("A")+i-1)           # Affiche les coordonnées x (axe des abscisses) 
+        t[3][i*2+1] = variables_pieces.pion_n      # Dessine Pion Noir
+        t[13][i*2+1] = variables_pieces.pion_b     # Dessine Pion Blanc
+    t[15][3] = variables_pieces.tour_b         # Dessine Tour Blanche Gauche
+    t[15][17] = variables_pieces.tour_b        # Dessine Tour Blanche Droite
+    t[15][5] = variables_pieces.cavalier_b     # Dessine Cavalier Blanc Gauche
+    t[15][15] = variables_pieces.cavalier_b    # Dessine Cavalier Blanc Droite
+    t[15][7] = variables_pieces.fou_b          # Dessine Fou Blanc Gauche
+    t[15][13] = variables_pieces.fou_b         # Dessine Fou Blanc Droite
+    t[15][9] = variables_pieces.reine_b        # Dessine Reine Blanche
+    t[15][11] = variables_pieces.roi_b         # Dessine Roi Blanc
+    t[1][3] = variables_pieces.tour_n          # Dessine Tour Noir Gauche
+    t[1][17] = variables_pieces.tour_n         # Dessine Tour Noir Droite
+    t[1][5] = variables_pieces.cavalier_n      # Dessine Cavalier Noir Gauche
+    t[1][15] = variables_pieces.cavalier_n     # Dessine Cavalier Noir Droite
+    t[1][7] = variables_pieces.fou_n           # Dessine Fou Noir Gauche
+    t[1][13] = variables_pieces.fou_n          # Dessine Fou Noir Droite
+    t[1][9] = variables_pieces.roi_n           # Dessine Roi Noir
+    t[1][11] = variables_pieces.reine_n        # Dessine Reine Blanche
+
+def tour_joueur():
+    joueur1 = input("Nom Joueur 1 : ")
+    joueur2 = input("Nom Joueur 2 : ")
+    prochain_tour = joueur1
+    if prochain_tour == joueur1:
+        print(f"C'est le tour de {prochain_tour}")
+        prochain_tour = joueur2
+
+    elif prochain_tour == joueur2:
+        print(f"Tour de {prochain_tour}")
+        prochain_tour = joueur1
 
 def afficher_echiquier(t):
     """Affiche l'échiquier
 
     Args:
-        t (str): Tableau
+        t (list): Tableau à 2 dimensions
     """
-    for x in range(19):                       # Répéter 19 fois :
-        for y in range(19):                       # Répéter 19 fois :
-            print(t[x][y], end=" ")                   # Imprimer tableau ligne du tableau t
-        print()                                   # Imprimer Saut de ligne
-    return t                                  # Renvoyer t
+    for x in range(19):             # Répéter 19 fois :
+        for y in range(19):             # Répéter 19 fois :
+            print(t[x][y], end=" ")         # Imprimer tableau ligne du tableau t
+        print()                         # Imprimer Saut de ligne
+    tour_joueur()                   # Appel de la fonction tour_joueur
+    return t                        # Renvoyer t
 
 # TEST :
 creer_echiquier(t)    # Crée un échiquier avec les pièces à leur positions par défaut
@@ -145,22 +158,42 @@ dico_coords = {         # Base de données contenant toutes les coordonnées pos
 # TEST :
 print(dico_coords["D"]["1"])
 
-def deplacement(pos1, pos2):
-    pos2 = pos1
-    pos1 = " "
-    print("Le pion ",pos2," a bougé")
+def est_majuscule(caractere):
+    """Vérifie si un caractère est majuscule ou non
 
-"""  
-#dico_coords["3"]["A"] = dico_coords["1"]["A"]
-#dico_coords["1"]["A"] = " "  
-#t[9][3] =t[15][3]
-#t[15][3] = " "
-print(dico_coords["1"]["A"])
-bouger(dico_coords["1"]["A"], dico_coords["4"]["A"]) #change les valeaurs dans le dico mais pas dans le tableau t 
-afficher_echiquier(t)
+    Args:
+        caractere (str): UTF-8
 
+    Returns:
+        (bool): True or False
+    """
+    return "A" <= caractere <= "Z" # Vérifie si l'encodage UTF-8 du caractère est entre A et Z
 
-# synchronise le tableau t avec diccod pour que les modif de dicood soit effectuer sur le tableau t
+def est_chiffre(caractere):
+    """Vérifie si un caractère est chiffre ou non
 
-#revenir au concret quand on est trop dans l'abstrait
-"""
+    Args:
+        caractere (str): UTF-8
+
+    Returns:
+        (bool): True of False
+    """
+    return '0' <= caractere <= '9' # Vérifie si l'encodage UTF-8 du caractère est entre 0 et 9
+    
+def reco_coordonnees(coordonnees):
+    """Interprète une chaine de caractère pour déterminer la position dans le dictionnaire
+
+    Args:
+        coordonnees (str): x et y
+    
+    Returns:
+        (list): Dictionnaire à 2 dimensions 
+    """
+    x = coordonnees[0]       # Attribue le premier caractere de la chaine à la variable x
+    y = coordonnees[1]       # Attribue le deuxième caractere de la chaine à la variable y
+    est_majuscule(x)         # Vérifie si y est un nombre
+    est_chiffre(y)           # Vérifie si y est un chiffre
+    return dico_coords[x][y] # Renvoie les indices correspondant aux coordonnées
+
+# TEST :
+print(reco_coordonnees("D8"))
